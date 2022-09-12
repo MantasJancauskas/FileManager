37a2652cba9eed460c0c64bc211e8f0f265ddb55
MantisJ <106962766+MantisJ@users.noreply.github.com>
2022-09-12 12:37:08 +0300

    Fixed refresh reupload bug, when delete file from directory in pc when you refresh it reuploads the same file

--- index.php
+++ index.php
@@ -209,6 +209,12 @@ print('</table>');
                                             : print('?' . implode('/', $q_string) . '/');
                                         ?>" class="navButton">Go Back!</a>
     </nav>
+    <script>
+    if ( window.history.replaceState ) {
+        window.history.replaceState( null, null, window.location.href );
+    }
+</script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
 </body>
